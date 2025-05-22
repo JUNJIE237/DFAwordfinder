08dca80a95d2b26178a53688e61b3e9c8db212a1
Dean Lau <ahdeanlau@gmail.com>
2025-05-22 09:35:32 +0800

    import dfa

--- dfa_place_finder/__init__.py
+++ dfa_place_finder/__init__.py
@@ -5,5 +5,6 @@ Public re-exports so *app.py* can simply do:
     from dfa_highlighter import build_dfa, scan_paragraph
 """
 from .processor import build_dfa, scan_paragraph
+from .dfa import DFA
 
-__all__ = ["build_dfa", "scan_paragraph"]
+__all__ = ["build_dfa", "scan_paragraph", "DFA"]
